ac4b75b97a1916de5a57168e10199120df2eb384
Andrea Grillo <andrea.grillo@epfl.ch>
2023-11-19 12:19:19 +0100

    global navigation implementation

--- global_navigation.py
+++ global_navigation.py
@@ -1,116 +1,115 @@
 import sys
- 
+import math
+
 class Graph(object):
-    def __init__(self, nodes, init_graph):
+    def __init__(self, nodes):
         self.nodes = nodes
-        self.graph = self.construct_graph(nodes, init_graph)
-        
-    def construct_graph(self, nodes, init_graph):
-        '''
-        This method makes sure that the graph is symmetrical. In other words, if there's a path from node A to B with a value V, there needs to be a path from node B to node A with a value V.
-        '''
-        graph = {}
+        self.graph = {}
         for node in nodes:
-            graph[node] = {}
-        
-        graph.update(init_graph)
-        
-        for node, edges in graph.items():
-            for adjacent_node, value in edges.items():
-                if graph[adjacent_node].get(node, False) == False:
-                    graph[adjacent_node][node] = value
-                    
-        return graph
+            self.graph[node] = {}
+
+    def add_node(self,a):
+        if not isinstance(a,tuple) or len(a) != 2:
+            print("ERROR - wrong parameters to function add_node")
+            exit(-1)
+        self.graph[a] = {}
+
+
+    def add_edge(self,a,b):
+        if not isinstance(a,tuple) or not isinstance(b,tuple) or len(a) != 2 or len(b) != 2:
+            print("ERROR - wrong parameters to function add_edge")
+            exit(-1)
+
+        my_dist = self.dist(a,b)
+        self.graph[a][b] = my_dist
+        self.graph[b][a] = my_dist
     
     def get_nodes(self):
-        "Returns the nodes of the graph."
-        return self.nodes
+        return self.nodes.copy()
     
-    def get_outgoing_edges(self, node):
-        "Returns the neighbors of a node."
+    def dist(self, a, b):
+        if not isinstance(a,tuple) or not isinstance(b,tuple) or len(a) != 2 or len(b) != 2:
+            print("ERROR - wrong parameters to function value")
+            exit(-1)
+        return math.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)   
+     
+    def get_outgoing_edges(self, a):
+        if not isinstance(a,tuple) or len(a) != 2:
+            print("ERROR - wrong parameters to function add_node")
+            exit(-1)
+
         connections = []
         for out_node in self.nodes:
-            if self.graph[node].get(out_node, False) != False:
+            if self.graph[a].get(out_node, False) != False:
                 connections.append(out_node)
         return connections
-    
-    def value(self, node1, node2):
-        "Returns the value of an edge between two nodes."
-        return self.graph[node1][node2]
 
-def dijkstra_algorithm(graph, start_node):
-    unvisited_nodes = list(graph.get_nodes())
- 
-    # We'll use this dict to save the cost of visiting each node and update it as we move along the graph   
-    shortest_path = {}
- 
-    # We'll use this dict to save the shortest known path to a node found so far
-    previous_nodes = {}
- 
-    # We'll use max_value to initialize the "infinity" value of the unvisited nodes   
-    max_value = sys.maxsize
-    for node in unvisited_nodes:
-        shortest_path[node] = max_value
-    # However, we initialize the starting node's value with 0   
-    shortest_path[start_node] = 0
-    
-    # The algorithm executes until we visit all nodes
-    while unvisited_nodes:
-        # The code block below finds the node with the lowest score
-        current_min_node = None
-        for node in unvisited_nodes: # Iterate over the nodes
-            if current_min_node == None:
-                current_min_node = node
-            elif shortest_path[node] < shortest_path[current_min_node]:
-                current_min_node = node
-                
-        # The code block below retrieves the current node's neighbors and updates their distances
-        neighbors = graph.get_outgoing_edges(current_min_node)
-        for neighbor in neighbors:
-            tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
-            if tentative_value < shortest_path[neighbor]:
-                shortest_path[neighbor] = tentative_value
-                # We also update the best path to the current node
-                previous_nodes[neighbor] = current_min_node
- 
-        # After visiting its neighbors, we mark the node as "visited"
-        unvisited_nodes.remove(current_min_node)
-    
-    return previous_nodes, shortest_path
+    def dijkstra(self, start_node):
+        unvisited_nodes = self.get_nodes()
 
-def print_result(previous_nodes, shortest_path, start_node, target_node):
-    path = []
-    node = target_node
+        # cost of getting to that node so far
+        shortest_path = {}
     
-    while node != start_node:
-        path.append(node)
-        node = previous_nodes[node]
- 
-    # Add the start node manually
-    path.append(start_node)
+        # previous node to get to that node in the optimal path, so far
+        previous_nodes = {}
     
-    print("We found the following best path with a value of {}.".format(shortest_path[target_node]))
-    print(" -> ".join(reversed(path)))
-
-nodes = ["Reykjavik", "Oslo", "Moscow", "London", "Rome", "Berlin", "Belgrade", "Athens"]
- 
-init_graph = {}
-for node in nodes:
-    init_graph[node] = {}
+        # max_value to initialize "infinity" value of unvisited nodes   
+        max_value = sys.maxsize
+        for node in unvisited_nodes:
+            shortest_path[node] = max_value
+        # starting node value = 0   
+        shortest_path[start_node] = 0
+        
+        # The algorithm executes until we visit all nodes
+        while len(unvisited_nodes) != 0:
+            # The code block below finds the node with the lowest score
+            current_min_node = unvisited_nodes[0]
+            for node in unvisited_nodes: # Iterate over the nodes
+                if shortest_path[node] < shortest_path[current_min_node]:
+                    current_min_node = node
+                    
+            # The code block below retrieves the current node's neighbors and updates their distances
+            neighbors = graph.get_outgoing_edges(current_min_node)
+            for neighbor in neighbors:
+                tentative_value = shortest_path[current_min_node] + graph.dist(current_min_node, neighbor)
+                if tentative_value < shortest_path[neighbor]:
+                    shortest_path[neighbor] = tentative_value
+                    previous_nodes[neighbor] = current_min_node
     
-init_graph["Reykjavik"]["Oslo"] = 5
-init_graph["Reykjavik"]["London"] = 4
-init_graph["Oslo"]["Berlin"] = 134
-init_graph["Oslo"]["Moscow"] = 3
-init_graph["Moscow"]["Belgrade"] = 5
-init_graph["Moscow"]["Athens"] = 4
-init_graph["Athens"]["Belgrade"] = 1
-init_graph["Rome"]["Berlin"] = 2
-init_graph["Rome"]["Athens"] = 2
+            # After visiting its neighbors, we mark the node as "visited"
+            unvisited_nodes.remove(current_min_node)
+        
+        return previous_nodes, shortest_path
+    
+    def path_planning(self,start_node,target_node):
+        previous_nodes, shortest_path = graph.dijkstra(start_node)
+        node = target_node
+        path = []
+        while node != start_node:
+            path.append(node)
+            node = previous_nodes[node]
+        path.reverse()
 
-#print(init_graph)
+        print("We found the following best path with a value of {}.".format(shortest_path[target_node]))
+        print(str(start_node) + " -> " + " -> ".join(str(a) for a in path))
 
-graph = Graph(nodes, init_graph)
-previous_nodes, shortest_path = dijkstra_algorithm(graph=graph, start_node="Reykjavik")
-print_result(previous_nodes, shortest_path, start_node="Reykjavik", target_node="Belgrade")
+        return path
 
+
+nodes = [(1,2),(3,4),(5,6),(10,11)]
+
+graph = Graph(nodes)
+
+graph.add_edge((1,2),(3,4))
+graph.add_edge((1,2),(5,6))
+graph.add_edge((3,4),(10,11))
+graph.add_edge((10,11),(5,6))
+
+
+start_node = (1,2)
+target_node= (10,11)
+
+
+path = graph.path_planning(start_node,target_node)
+
+print(path)
