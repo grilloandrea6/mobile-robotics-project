7fe58125b870b55b1dfad02897bf916ae165917a
Andrea Grillo <andrea.grillo@epfl.ch>
2023-11-22 16:01:45 +0100

    local

--- local_navigation.py
+++ local_navigation.py
@@ -19,25 +19,33 @@ CONV_RATIO = 65
 # 
 THRESHOLD = 6000
 
+KP_ALPHA = .5
+KP_BETA = .5
+
+#TODO TODO TODO
+theta_goal = 0.5
+
 class Local_Navigation(object):
+  #def __init__(self, path):
+  #  print("init of the class")
+
   def __init__(self, path):
     self.path = path
     self.waypoint_counter = 0
     self.state = 0 # 0 -> path following / 1 -> obstacle avoidance
+
   
-  def dist(x,y):
-    return math.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)     
-
-  def control(pose, sensor_data):
+  def control(self,pose, sensor_data):
     if not self.present_obstacle(sensor_data):
-      v,w = path_follow(pose)
-      return differential_steering(v,w)
+      v,w = self.path_follow(pose)
+      return self.differential_steering(v,w)
 
-    else return obstacle_avoidance(sensor_data)
+    else:
+      return obstacle_avoidance(sensor_data)
 
 
-  def present_obstacle(sensor_data):
-    if(self.state == 0) 
+  def present_obstacle(self,sensor_data):
+    if self.state == 0 :
       # we have been following the path until now, we have to check if there's an obstacle
       
       # we want to give more weight to the front sensor (2) as we do not want       
@@ -47,43 +55,44 @@ class Local_Navigation(object):
         return True
       else:
         return False
-
-    else
-      
-
-
-
+    else:
+      # we have been avoiding obstacles, we have to check if we can go back to path following
+      #TODO
+      return False
 
 
-  def path_follow(pose)
+  def path_follow(self,pose):
     #pose should be a (x,y,theta) tuple
+
 
     objective = self.path[self.waypoint_counter]
 
-    distance = dist(pose,objective)
+    distance = self.dist(pose,objective)
 
-    # check if we reached the objective
-    if distance < eps:
-      self.waypoint_counter ++
+    # check if we reached the objective - non mi piace proprio, voglio verificare se ho superato la retta normale al percorso che volevo fare
+    if distance < EPS:
+      self.waypoint_counter+=1
       objective = self.path[self.waypoint_counter]
-      distance = dist(pose,objective)
-
     
     x_diff, y_diff = objective[0] - pose[0], objective[1] - pose[1]
-
+    theta = pose[2]
         
     alpha = (math.atan2(y_diff, x_diff) - theta + math.pi) % (2 * math.pi) - math.pi
     beta = (theta_goal - theta - alpha + math.pi) % (2 * math.pi) - math.pi
 
 
-    w = Kp_alpha * alpha + Kp_beta * beta
+    w = KP_ALPHA * alpha + KP_BETA * beta
 
     v = VEL if alpha > math.pi / 2 or alpha < -math.pi / 2 else -VEL
 
-    return self.diff_eq(v,w)
+    return (v,w)
   
-  def differential_steering(v,w)
+  def differential_steering(self,v,w):
     wl = CONV_RATIO * (2*v - w*L)/(2*R) # left motor speed
     wr = CONV_RATIO * (2*v + w*L)/(2*R) # right motor speed
     
     return wl, wr
+
+  def dist(self,a,b):
+    return math.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)     
+
