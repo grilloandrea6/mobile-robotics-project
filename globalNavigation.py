9d914f12d7565665a9a047c26e7451595a701071
BadilM <126114677+BadilM@users.noreply.github.com>
2023-11-25 11:07:33 +0100

    Added working global nav and vision

---
+++ globalNavigation.py
@@ -0,0 +1,110 @@
+import numpy as np
+import cv2
+from math import dist
+import pyvisgraph as vg
+
+def prepareImage(frame, threshold = 100):
+    kernel = np.ones((2,2),np.uint8)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)   
+    
+    gray = cv2.GaussianBlur(gray, (3, 3), 0)
+    gray = cv2.medianBlur(gray, 3)
+
+    _ ,thresh = cv2.threshold(gray, threshold,255,0)
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_GRADIENT, kernel)
+    thresh = cv2.dilate(thresh,kernel,iterations = 5)
+        
+    return thresh
+
+def approx_contour(contour, epsilon = 10.0, closed = True):
+    contour_approx = cv2.approxPolyDP(contour, epsilon = 10.0, closed = True)
+    return contour_approx
+
+def contourCenter(contour):        
+    M = cv2.moments(contour)
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+    center = [cX, cY]
+    return center
+
+def getLength(p1, p2):
+    length = dist(p1, p2)
+    return length   
+
+def getDilatationScale(d1, d2):
+    return d1/d2
+
+def scalePointsFromCenter(points, center, length):
+    for p in range(points.shape[0]):
+        distCenter = getLength(center, [points[p,0,0], points[p,0,1]] )
+        scale = getDilatationScale(distCenter+length, distCenter)
+        
+        # Translate to center
+        points[p, 0, :] = points[p, 0, :] - center 
+        # Scale
+        points[p, 0, :] = points[p,0,:]*scale
+        # Translate back to center
+        points[p, 0, :] = points[p, 0, :] + center
+    return points
+
+def drawSimplifiedContours(contours, img):
+    listContour = list()
+    if len(contours) > 0:
+        for cont in contours:
+            # We try to find the small contours to delete them
+            # by looking at the smallest square area that they can fill
+            [_ , (height, width), _] = cv2.minAreaRect(cont)
+            area = height*width
+            if area < 2500: # equivalent to a square of 50x50
+                continue
+            
+            # Approximation on contour into few points and scaling of those points
+            contour_approx = approx_contour(cont)
+            
+            # There's a problem with the center calculation
+            #contour_scaled = scalePointsFromCenter(contour_approx, contourCenter(contour_approx), 50)
+            
+            listContour.append(contour_approx[:,0,:])
+            cv2.drawContours(img, contour_approx, -1, (0,0,255), 10)
+    else:
+        print("No contour found")
+    
+    return img, listContour
+
+def addStartAndGoal(img, contourList, start, goal, scalingFactor):
+    y, x = img.shape[0:2]
+    print(contourList)
+    start = np.array(([(start/scalingFactor)]),np.int32)
+    goal = np.array(([(goal/scalingFactor)]),np.int32)
+    
+    # We add the goal and start points 
+    contourList.append(goal) # Goal
+    contourList.insert(0, start)  # Start
+    
+    print(contourList)
+    return img, contourList
+
+def findShortestPath(contourList):
+    polyList = list()
+    poly = list()   
+    
+    #Goal is not appended right in the poly list
+    for obstacle in contourList[:]:
+        polyList = list()
+        for i in range(len(obstacle)):
+            polyList.append(vg.Point(obstacle[i,0], obstacle[i,1]))
+            
+        poly.append(polyList)
+
+    print(poly)
+    g = vg.VisGraph()
+    g.build(poly[1:-2], status = False)
+    shortest = g.shortest_path(vg.Point(contourList[0][0,0], contourList[0][0,1]), vg.Point(contourList[-1][0,0], contourList[-1][0,1]))
+
+    shortestPath = list()
+    for p in shortest:
+        shortestPath.append(np.array([p.x, p.y]))
+    path = np.array(shortestPath).astype(int)
+    
+    return path
---
+++ visionThymio.py
@@ -0,0 +1,283 @@
+import numpy as np
+import cv2
+import time
+import globalNavigation as gn
+### Markers detection functions
+
+# We use IDs: - 1 for the direction of the thymio
+#             - 2 for the goal
+#             - 3 and 4 to get the scaling factor of the distances by knowing the distance in meters
+    
+def initDictAruco(type = cv2.aruco.DICT_4X4_50):
+    dict = cv2.aruco.getPredefinedDictionary(type)
+    return dict
+
+def detectArucoMarkers(img, dict):
+    marker_ids = []
+    corners = []
+    
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Corners are given in clockwise order starting with top left 
+    corners, ids, _ = cv2.aruco.detectMarkers(image=img, dictionary=dict)
+    
+    if len(corners) != 0:
+        for i in range(len(ids)):
+            marker_ids.append(ids[i][0])
+                
+    return marker_ids, corners
+
+
+# Corrected image acquisition with camera Matrix
+def getFrame(cap):
+    
+    # These are the values used to correct different types of opticals aberations
+    # such as the spherical aberation or the tangential one
+    # These values have been found after following a valibration process
+    # (opencv camera calibration with charuco patterns)
+    
+    mtx = np.array([[989.7401734,    0.,         653.72226958],
+            [  0.,         998.64802591, 347.61109886],
+            [  0.,           0.,           1.        ]])
+    dist = np.array([[ 0.12258701, -0.76974406, -0.00790144,  0.00456124,  1.16348775]])
+    
+    _, frame = cap.read()
+    
+    h,  w = frame.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+    
+    dst = cv2.undistort(frame, mtx, dist, None, newcameramtx)
+    # crop the image
+    x, y, w, h = roi
+    dst = dst[y:y+h, x:x+w]
+    return dst
+
+# We compute a scaling factor by dividing the distance
+# between two Arcuo Markers measured in px and in meters
+
+# The perspective transformation is found by takeing 4 corners of the ROI
+# We can then map our image to a new one with corrected perspective
+def getPerspectiveAndScaling(cap, dict, width_roi=48.4, height_roi= 31):
+    
+    # Need to measure the size of the ROI in real life
+    ratio_vh = width_roi/height_roi
+    transformMatrix = []
+    
+    # loops until good transform matrix found 
+    while len(transformMatrix) == 0:
+        _ , img = cap.read()
+        ids, corners = detectArucoMarkers(img, dict)
+        wmax = 0
+        hmax = 0
+        
+        if ids != [] and (3 in ids) and (4 in ids) and (5 in ids) and (6 in ids):
+            
+            # Take the points of each marker closest to ROI
+            mark4 = corners[ids.index(4)][0]
+            mark3 = corners[ids.index(3)][0]
+            mark5 = corners[ids.index(5)][0]
+            mark6 = corners[ids.index(6)][0]
+            
+            pointA = mark4[2,:]
+            pointB = mark3[1,:]
+            pointC = mark5[0,:]
+            pointD = mark6[3,:]
+
+            # Warning, the point corresponding must be the closest to the  -->  to verify
+            inputPoints = np.array([pointA, pointB, pointC, pointD],np.float32)
+
+            
+            hmax = img.shape[1]
+            wmax = hmax*ratio_vh
+            
+            outputPoints = np.array([[0, 0],
+                            [0, hmax],
+                            [wmax, hmax],
+                            [wmax, 0]], np.float32)
+            
+            
+            img = cv2.circle(img, pointA.astype(int), radius=0, color= (0,0,255), thickness = 5)
+            img = cv2.circle(img, pointB.astype(int), radius=0, color= (0,255,0), thickness = 5)
+            img = cv2.circle(img, pointC.astype(int), radius=0, color= (255,0,0), thickness = 5)
+            img = cv2.circle(img, pointD.astype(int), radius=0, color= (120,120,120), thickness = 5)
+            
+            # delete
+            cv2.imshow("point for ROI", img)
+            while True:
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break 
+            
+            transformMatrix = cv2.getPerspectiveTransform(inputPoints, outputPoints)
+            
+            sizeROI = np.array([hmax, wmax]).astype(int)
+            
+            scalingFactor = height_roi/hmax
+        else:
+            transformMatrix = []
+            print("Can't find markers 3,4, 5 and 6 for perspective transformation")
+
+    return transformMatrix, sizeROI, scalingFactor
+
+def getCorrectedImage(cap, transformMatrix, sizeROI):
+    img = getFrame(cap)
+    img = cv2.warpPerspective(img, transformMatrix ,(sizeROI[1], sizeROI[0]),flags=cv2.INTER_LINEAR)
+    # Need to correct the perspective transformation 
+    
+    return img
+    
+    
+def getGoalPosition(cap, dict, transformMatrix,scalingFactor, sizeROI):    
+    position = []
+    
+    # If some aruco patterns are detected, verify if 2 are among them
+    while len(position) == 0:
+        img = getCorrectedImage(cap, transformMatrix, sizeROI)
+        ids, corners = detectArucoMarkers(img, dict)
+
+        input("Place marker 2 and press ENRER when ready")        
+        
+        if (len(ids) != 0) and (2 in ids):
+            sum = [0,0]
+            nElem = 0
+            
+            mark = corners[ids.index(2)][0]
+
+            # Position is the mean value of each corner of the marker
+            position = (np.sum(mark, 0)/4).astype(int)
+        else:
+            print("Can't find goal marker (2) !")
+    
+    return position*scalingFactor
+
+def getThymioPos(img, ids, corners, scalingFactor):
+        
+    position = []
+    orientation = []
+    
+    # If some aruco patterns are detected, verify if 1 are among them
+    if len(ids) != 0 and 1 in ids:
+            sum = [0,0]
+            nElem = 0
+            
+            mark = corners[ids.index(1)][0]
+
+            # Position is the mean value of each corner of the marker
+            position = (np.sum(mark, 0)/4).astype(int)
+            
+            # Vector for the direction of the thymio (from two left corners of the mark)
+            orientation = mark[0,:]-mark[-1,:]
+            orientation = orientation.astype(int) 
+            
+            img = cv2.circle(img, position, radius = 0, color = (0,0,255), thickness=5)
+            img = cv2.line(img,mark[0,:].astype(int), mark[-1,:].astype(int), color = (255,0,0), thickness=2)
+            
+            position = (position*scalingFactor)
+            
+    return position, orientation, img
+
+# ========================================================
+
+def startVideoCapture():
+    cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+    cap.set(3,854)
+    cap.set(4,480)
+    return cap
+
+def stopVideoCapture(cap):
+    cap.release()
+    cv2.destroyAllWindows()
+    
+# ========================================================
+
+# Need to find the visibility graph without the goal and start positions
+def getVisibilityGraph(cap, transformMatrix, sizeROI):
+    
+    img = getCorrectedImage(cap, transformMatrix, sizeROI)
+    
+    thresh = gn.prepareImage(img)
+    
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        
+    frame, contourList = gn.drawSimplifiedContours(contours, img)
+    
+    # delete
+    cv2.imshow("ContourList",frame)
+    while True:
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()
+
+    return contourList
+
+def getOptimalPath(cap, transformMatrix, dict, staticObstacleList, goal, sizeROI, scalingFactor):
+    path = []
+    
+    # We look at the initial position of the thymio to find the optimal path
+    start = []
+    while len(start) == 0 :
+        
+        img = getCorrectedImage(cap, transformMatrix, sizeROI)
+        ids, corners = detectArucoMarkers(img, dict)
+        
+        input("Place the robot with marker 1 and press ENTER when ready!")
+        start, _ , _ = getThymioPos(img, ids, corners, scalingFactor)
+        
+        if (len(start) == 0):
+            print("Coudn't find the robot marker, retry !")
+
+    _, staticObstacleList = gn.addStartAndGoal(img, staticObstacleList, start, goal, scalingFactor)
+
+    path = gn.findShortestPath(contourList = staticObstacleList)
+    
+    for i in range(len(path)-1):
+        img = cv2.line(img, path[i, :], path[i+1, :], color=(0, 0, 255), thickness=2)
+    
+    return path
+
+
+#====================================================================
+#====================================================================
+#====================================================================
+
+# initialize variables
+cap = startVideoCapture()
+dict = initDictAruco()
+
+time.sleep(2)
+
+# get values for image correction (persepctive and scaling)
+transformMatrix, sizeROI, scalingFactor = getPerspectiveAndScaling(cap, dict)
+
+# Then we need to do the computing fot visibility graph and optimal path
+# We can compute the visibility graph first and find the static obstacles
+staticObstacleList = getVisibilityGraph(cap, transformMatrix, sizeROI)
+
+# Then we get the goal position
+goal = getGoalPosition(cap, dict, transformMatrix, scalingFactor, sizeROI)
+
+# Finally, we compute the optimal path
+path = getOptimalPath(cap, transformMatrix, dict, staticObstacleList, goal, sizeROI, scalingFactor)
+# Only this last function is needed to recompute the optimal path
+
+print(path)
+
+while(True):
+    # Capture frame-by-frame
+    img = getCorrectedImage(cap, transformMatrix, sizeROI)
+    
+    
+    # How to get the thymios position
+    ids, corners = detectArucoMarkers(img, dict)
+    pos, orient, _ = getThymioPos(img, ids, corners, scalingFactor)
+    
+    print(pos, orient)
+    
+    for i in range(len(path)-1):
+        img = cv2.line(img, path[i, :], path[i+1, :], color=(0, 0, 255), thickness=2) 
+    
+    # Display the resulting frame
+    cv2.imshow('frame',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+stopVideoCapture(cap)
