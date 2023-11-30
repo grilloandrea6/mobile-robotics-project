467aee54cf7c49a6d37273449ac5d927002ce9ef
Andrea Grillo <grilloandrea6@gmail.com>
2023-11-30 22:59:37 +0100

    documentation

---
+++ test/local_navigation.py
@@ -0,0 +1,114 @@
+import math
+
+# CONSTANT DEFINITION
+
+# distance to a waypoint to be considered reached
+EPS = .2
+
+# default constant speed
+VEL = 2000
+
+# radius of thymio wheels
+R = 20
+
+# distance between wheel centers
+L = 105
+
+# conversion ratio for wheel speed
+CONV_RATIO = 2
+
+# controller proportional constant for angular velocity
+KP_ALPHA = .6
+
+# weights for obstacle avoidance using proximity sensors
+WEIGHT_LEFT = [ 6,  9, -10,  -9, -6]
+WEIGHT_RIGHT = [-6, -9, -12.5, 9,  6]
+
+# base speed of the obstacle avoidance
+VEL_OBST_AVOID = 115
+
+# scale factors for proximity sensors
+SENSOR_SCALE = 230
+
+# END CONSTANT DEFINITION
+
+class Local_Navigation(object):
+  def __init__(self):
+    print("Init Local Navigation")
+
+  def define_path(self,path):
+    self.path = path
+    self.waypoint_counter = 0
+    self.obstacle_counter = 0
+    
+  def control(self, pose, sensor_data):
+    if self.present_obstacle(sensor_data):
+      self.obstacle_counter = 5
+
+    if self.obstacle_counter > 0 :
+      if not self.present_obstacle(sensor_data):
+        self.obstacle_counter -= 1
+
+      return self.obst_avoid(sensor_data), False
+      
+    else :
+      v,w = self.path_follow(pose)
+      return self.differential_steering(v,w), (v,w) == (-1,-1)
+
+  def obst_avoid(self,data):
+    wsx = wdx = 0
+
+    for i in range(len(data) - 2):
+        # Compute outputs of neurons and set motor powers
+        wsx = wsx + data[i] * WEIGHT_LEFT[i] / SENSOR_SCALE
+        wdx = wdx + data[i] * WEIGHT_RIGHT[i] / SENSOR_SCALE
+
+    return int(VEL_OBST_AVOID + wsx),int(VEL_OBST_AVOID + wdx)
+    
+  def path_follow(self, pose):
+    #pose is a (x,y,theta) tuple
+    thymio_angle = self.radToDeg(pose[2])
+    if self.waypoint_counter >= len(self.path):
+      return 0,0
+    objective = self.path[self.waypoint_counter]
+    x_diff, y_diff = objective[0] - pose[0], objective[1] - pose[1]
+    distance = self.dist(pose,objective)
+    angleFollow = self.radToDeg(math.atan2(y_diff,x_diff))
+
+    print(f"My pose is {pose[0]} {pose[1]} {thymio_angle}")
+    print(f"My objective is {objective}")
+    print(f"Distance is {distance}")
+    print(f"the angle i have to follow {angleFollow}")
+
+    # check if we reached the objective
+    if distance < EPS:
+      print(f"reached objective number {self.waypoint_counter}")
+      self.waypoint_counter+=1
+      if self.waypoint_counter >= len(self.path) :
+        print("finished!")
+        return (-1,-1)
+      else:
+        return self.path_follow(pose)
+      
+    # alpha is the delta between the orientation of the thymio and 
+    # the heading we have to follow to reach the objective
+    # scaled to be in the interval [-180,+180]
+    alpha = (thymio_angle - angleFollow + 180) % 360 - 180
+    print(f"DELTA ANGLE DEG: {alpha}")
+
+    return VEL, KP_ALPHA * alpha 
+  
+  def differential_steering(self, v, w):
+    wl = CONV_RATIO * (2*v - w*L)/(2*R) # left motor speed
+    wr = CONV_RATIO * (2*v + w*L)/(2*R) # right motor speed
+    
+    return int(wl), int(wr)
+
+  def dist(self,a,b):
+    return math.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
+    
+  def radToDeg(self,angle):
+    return angle * 180 / math.pi
+  
+  def present_obstacle(self,sensor_data):
+      return sum(sensor_data) > 0
--- test/local_navigation_test.py
+++ test/local_navigation_test.py
@@ -1,39 +1,37 @@
-import sys
 import math
-sys.path.append('../')
 from local_navigation import Local_Navigation
 import matplotlib.pyplot as plt 
 
 nodes = [(0,0),(1,0),(3,10),(5,5),(10,2),(12,7)]
 
 
-local_nav = Local_Navigation(nodes)
 
-#pose = []
-#pose.append((0,0,45* math.pi / 180))
+local_nav = Local_Navigation()
+
+local_nav.define_path(nodes)
+
 x = [0]
 y = [0]
-theta = 45 * math.pi / 180
+theta = 25 * math.pi / 180
 
-for i in range(20000):
+for i in range(1000):
     print(f"iterazione numero {i}")
     v,w = local_nav.path_follow((x[-1],y[-1],theta))
-
+    v = v/7.5
     wl,wr= local_nav.differential_steering(v,w)
     print(f"differential steering {(wl,wr)}") 
-    if abs(wl) > 4000 or abs(wr) > 4000:
+    if abs(wl) > 400 or abs(wr) > 400:
         print(f"SATURATED")
         #input()
     if (v,w) == (-1,-1):
         print("FINISHED")
         break
-    #pose.append(   (pose[-1][0] + v * math.cos(pose[-1][2])/400, pose[-1][1] + v * math.sin(pose[-1][2])/400, pose[-1][2] + w/400) )
-    x.append(x[-1] + v *  math.cos(theta)/400)
-    y.append( y[-1] + v * math.sin(theta)/400)
-    theta = theta + w/400
+    x.append(x[-1] + v *  math.cos(theta)/5000)
+    y.append( y[-1] + v * math.sin(theta)/5000)
+    theta = theta - w/400
     
 
-    plt.arrow(x[-1],y[-1],v*math.cos(theta)/600,v*math.sin(theta)/600)
+    plt.arrow(x[-1],y[-1],v*math.cos(theta)/2000,v*math.sin(theta)/2000)
 
 for i in range(len(nodes)):
     plt.scatter(nodes[i][0],nodes[i][1])
