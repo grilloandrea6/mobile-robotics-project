d6ec03f9876814b033d2cd7c6ab482788c8a988d
Andrea Grillo <grilloandrea6@gmail.com>
2023-11-30 16:57:46 +0100

    working on local navigation

--- local_navigation.py
+++ local_navigation.py
@@ -24,72 +24,87 @@ KP_ALPHA = 1
 KP_BETA = -.5
 KP_DIST = 70
 
+PATH_FOLLOWING = 0
+OBSTACLE_AVOIDANCE = 1
+
+ANGLE_THRESH = 5
+
 # END CONSTANT DEFINITION
 
 class Local_Navigation(object):
   def __init__(self):
     print("Init Local Navigation")
     self.waypoint_counter = 0
-    #self.state = 0 # 0 -> path following / 1 -> obstacle avoidance
+    self.state = PATH_FOLLOWING # 0 -> path following / 1 -> obstacle avoidance
 
   def define_path(self,path):
     self.path = path
     self.waypoint_counter = 0
     
   def control(self, pose, sensor_data):
-    if not self.present_obstacle(sensor_data):
+    if self.state == PATH_FOLLOWING:
+      if self.present_obstacle(sensor_data):
+        self.state == OBSTACLE_AVOIDANCE
+        return self.obst_avoid(sensor_data)
+
       v,w = self.path_follow(pose)
       return self.differential_steering(v,w)
     else:
+      _, _, angleFollow = self.getObjective(pose)
+      thymioAngle = self.radToDeg(pose[2])
+      if abs(angleFollow - thymioAngle) < ANGLE_THRESH and not self.present_obstacle(sensor_data):
+        self.state = PATH_FOLLOWING
+        v,w = self.path_follow(pose)
+        return self.differential_steering(v,w)
+      
       return self.obstacle_avoidance(sensor_data)
 
   def present_obstacle(self,sensor_data):
-      # we want to give more weight to the front sensor (2) as we do not want       
-      for i in range(len(sensor_data)):
-        sum += sensor_data[i]
-      
-      if sum > 0:
+      if sum(sensor_data) > 0:
         return True
       else:
         return False
 
 
-  def obst_avoid(self, data):
-    w_l = [6,  11, -10, -11, -6,  0,0]
-    w_r = [-6, -11, -12.5,  11,  6, 0,0]
+  def obst_avoid(self,data):
+    w_l = [ 6,  9, -10,  -9, -6]
+    w_r = [-6, -9, -12.5, 9,  6]
 
     # Scale factors for sensors
     sensor_scale = 230
 
-    y = [0,0]
-    x = [0,0,0,0,0,0,0]
+    wsx = wdx = 0
 
-    for i in range(len(x)):
-        # Get and scale inputs
-        x[i] = data[i] / sensor_scale
-        
+    for i in range(len(data) - 2):
         # Compute outputs of neurons and set motor powers
-        y[0] = y[0] + x[i] * w_l[i]
-        y[1] = y[1] + x[i] * w_r[i]
+        wsx = wsx + data[i] * w_l[i] / sensor_scale
+        wdx = wdx + data[i] * w_r[i] / sensor_scale
 
-    return int(115 + y[0]),int(115 + y[1])
+    return int(115 + wsx),int(115 + wdx)
 
   def radToDeg(self,angle):
     return angle * 180 / math.pi
 
+  def getObjective(self,pose):
+    objective = self.path[self.waypoint_counter]
+    x_diff, y_diff = objective[0] - pose[0], objective[1] - pose[1]
+    distance = self.dist(pose,objective)
+    angleFollow = self.radToDeg(math.atan2(y_diff,x_diff))
+    return objective, distance, angleFollow
+
+
+
   def path_follow(self, pose):
     # ASTOLFI CONTROLLER
     #pose is a (x,y,theta) tuple
-    objective = self.path[self.waypoint_counter]
-    distance = self.dist(pose,objective)
+    thymio_angle = self.radToDeg(pose[2])
 
-    x_diff, y_diff = objective[0] - pose[0], objective[1] - pose[1]
-    thymio_angle = pose[2]
+    objective, distance, angleFollow = self.getObjective(pose)
 
     print(f"My pose is {pose[0]} {pose[1]} {self.radToDeg(pose[2])}")
     print(f"My objective is {objective}")
     print(f"Distance is {distance}")
-    print(f"the angle i have to follow {self.radToDeg(math.atan2(y_diff,x_diff))}")
+    print(f"the angle i have to follow {angleFollow}")
 
     # check if we reached the objective
     if distance < EPS:
@@ -102,9 +117,8 @@ class Local_Navigation(object):
         return self.path_follow(pose)
       
     #alpha = (- math.atan2(y_diff,x_diff) + thymio_angle + 5*math.pi) % ( 2 * math.pi) - math.pi
-    diff_angle = self.radToDeg(math.atan2(y_diff,x_diff))
-    thymio_angle = self.radToDeg(thymio_angle)
-    alpha = (thymio_angle - diff_angle + 180) % 360 - 180
+    
+    alpha = (thymio_angle - angleFollow + 180) % 360 - 180
     print(f"AAAA - DELTA ANGLE DEG: {alpha}")
 
     beta = 0 #(thymio_angle + alpha + 180) % 360 - 180
